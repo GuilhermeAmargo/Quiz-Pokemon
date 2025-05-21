9e3fc815fe4d9b787985e85154d0c72a7428a0e9
GuilhermeAmargo <guilhermecamargosilva211@gmail.com>
2025-05-21 10:33:50 -0300

    feat: add comments

--- app/index.tsx
+++ app/index.tsx
@@ -3,73 +3,82 @@ import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 import axios from "axios";
 import { useFonts } from "expo-font";
 
-// Interface
 interface Pokemon {
   name: string;
   image: string;
 }
 
 export default function Index() {
+
+  // Fontes
   const [fontsLoaded] = useFonts({
     Pokemon: require("../assets/fonts/PokemonSolid.ttf"),
     PokemonGame: require("../assets/fonts/PokemonGame.ttf"),
   });
 
-  // States
+  // Sets
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
   const [options, setOptions] = useState<string[]>([]);
   const [loading, setLoading] = useState(true);
 
-  // Recuperar informações
+  // Encontrar Pokémon (Nome e Imagem)
   const fetchPokemon = async () => {
-    const randomId = Math.floor(Math.random() * 1025) + 1; // Gera um ID aleatório de 1 a 1025
+    const randomId = Math.floor(Math.random() * 151) + 1; 
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
     const fullImage = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${String(randomId).padStart(3, '0')}.png`;
     setPokemon({ name: response.data.name, image: fullImage });
-    fetchWrongOptions(response.data.name); // Busca as alternativas erradas
+    fetchWrongOptions(response.data.name); 
   };
 
-  // Recupera as alternativas erradas (3 erradas e 1 certa)
+  // Encontrar 3 opções erradas
   const fetchWrongOptions = async (correctName: string) => {
     let wrongNames = new Set<string>();
     while (wrongNames.size < 3) {
-      const randomId = Math.floor(Math.random() * 1025) + 1;
+      const randomId = Math.floor(Math.random() * 151) + 1;
       const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomId}`);
       if (response.data.name !== correctName) {
         wrongNames.add(response.data.name);
       }
     }
+
+    // Adiciona o nome correto e embaralha as opções
     const allOptions = [correctName, ...Array.from(wrongNames)];
-    setOptions(shuffleArray(allOptions)); // Embaralha as opções antes de setar no estado
+    setOptions(shuffleArray(allOptions)); 
     setLoading(false);
   };
 
-  // Função para embaralhar as opções
+  // Embaralhar opções
   const shuffleArray = (array: string[]) => {
     return array.sort(() => Math.random() - 0.5);
   };
 
-  // Quando o componente é montado, busca o Pokémon aleatório
+  // Carregar Pokémon
   useEffect(() => {
     fetchPokemon();
   }, []);
 
+  // Verificar resposta
   const handleOptionPress = (selectedName: string) => {
     if (selectedName === pokemon?.name) {
-      alert("Correto! Vamos para o próximo Pokémon.");
-      setLoading(true); // Reseta o carregamento
-      fetchPokemon(); // Busca um novo Pokémon
+      alert("Correto!");
+      setLoading(true); 
+      fetchPokemon(); 
     } else {
-      alert("Errado! Tente novamente.");
+      alert("Errado!");
+      setLoading(true);
+      fetchPokemon();
     }
   };
 
+  // Verifica se as fontes
   if (!fontsLoaded || loading) {
     return <View />;
   }
 
   return (
     <View style={styles.container}>
+
+      {/* Verifica se o Pokémon foi carregado */}
       {pokemon && (
         <Image source={{ uri: pokemon.image }} style={styles.pokemonImage} />
       )}
@@ -77,6 +86,8 @@ export default function Index() {
       <Text style={styles.title}>Qual é esse Pokémon?</Text>
 
       <View style={styles.optionsContainer}>
+
+        {/* Opções carregadas */}
         {options.map((option, index) => (
           <TouchableOpacity
             key={index}
